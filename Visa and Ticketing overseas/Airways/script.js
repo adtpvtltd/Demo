c43814f849db68140602bcdb932df779bc5c1055
Aashish <165255153+ashishverma4619@users.noreply.github.com>
2024-05-25 15:07:34 +0530

    transfering the website

--- Visa and Ticketing overseas/Airways/script.js
+++ Visa and Ticketing overseas/Airways/script.js
@@ -85,4 +85,31 @@ window.addEventListener('click', function(event) {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Get the modal
+    var modal = document.getElementById("contactModal");
 
+    // Get the button that opens the modal
+    var contactLink = document.querySelector("header .menu a[href='#CONTACT']");
+
+    // Get the <span> element that closes the modal
+    var closeButton = document.querySelector(".close-button");
+
+    // When the user clicks the button, open the modal
+    contactLink.addEventListener("click", function(event) {
+        event.preventDefault();
+        modal.style.display = "block";
+    });
+
+    // When the user clicks on <span> (x), close the modal
+    closeButton.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.addEventListener("click", function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});
