431e0b7bace7e444ad13ae393d4362d8f6a31786
Aashish <165255153+ashishverma4619@users.noreply.github.com>
2024-07-24 10:42:43 +0530

    transfering the website

--- luminous-react/luminous/src/components/Navbar.jsx
+++ luminous-react/luminous/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import cart from './image/cart.png';
 
 function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isSearchModalOpen, setIsSearchModalOpen] = useState(false);
 
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
@@ -16,10 +17,19 @@ function Navbar() {
     setIsNavOpen(false);
   };
 
+  const openSearchModal = () => {
+    setIsSearchModalOpen(true);
+  };
+
+  const closeSearchModal = () => {
+    setIsSearchModalOpen(false);
+  };
+
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (!event.target.closest('.navbar')) {
         closeNav();
+        closeSearchModal();
       }
     };
     
@@ -30,29 +40,41 @@ function Navbar() {
   }, []);
 
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <ul className={`nav-links ${isNavOpen ? 'nav-open' : ''}`}>
-        <li><a href="/">HOME</a></li>
-        <li><a href="/">FEATURE</a></li>
-        <li><a href="/">SHOP</a></li>
-        <li><a href="/">PRODUCT</a></li>
-        <li><a href="/">PAGES</a></li>
-        <li><a href="/">BLOG</a></li>
-      </ul>
-      <div className="icons">
-        <img src={search} alt="search" />
-        <img src={setting} alt="setting" />
-        <img src={cart} alt="cart" />
-        <div className="hamburger" onClick={toggleNav}>
-          <div></div>
-          <div></div>
-          <div></div>
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <img src={logo} alt="logo" />
         </div>
-      </div>
-    </nav>
+        <ul className={`nav-links ${isNavOpen ? 'nav-open' : ''}`}>
+          <li><a href="/">HOME</a></li>
+          <li><a href="/">FEATURE</a></li>
+          <li><a href="/">SHOP</a></li>
+          <li><a href="/">PRODUCT</a></li>
+          <li><a href="/">PAGES</a></li>
+          <li><a href="/">BLOG</a></li>
+        </ul>
+        <div className="icons">
+          <img src={search} alt="search" onClick={openSearchModal} />
+          <img src={setting} alt="setting" />
+          <img src={cart} alt="cart" />
+          <div className="hamburger" onClick={toggleNav}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </nav>
+
+      {isSearchModalOpen && (
+        <div className="search-modal">
+          <div className="search-modal-content">
+            <h2>Search by Keywords</h2>
+            <input type="text" placeholder="Enter keywords..." />
+            <button onClick={closeSearchModal}>Close</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
