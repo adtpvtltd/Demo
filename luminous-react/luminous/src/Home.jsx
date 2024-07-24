7c7aff9864e53f2f5e327c560cb3e254b14f2501
Aashish <165255153+ashishverma4619@users.noreply.github.com>
2024-07-24 10:44:31 +0530

    transfering the website

---
+++ luminous-react/luminous/src/Home.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Banner from './components/Banner'
+import Jewelry from './components/Jewelry'
+import CategoryBar from './components/CategoryBar'
+import TopCollection from './components/collection/Collection'
+import PromoBanner from './components/PromoBanner'
+import ServiceBar from './components/ServiceBar'
+import CombinedComponent from './components/Carousal'
+import Layout from './components/Layout'
+import AdBanner from './components/Adbanner'
+import Social from './components/Social'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function Home() {
+  return (
+    <>
+    <Header/>
+    <Navbar/>
+    <Banner/>
+    <Jewelry/>
+    <CategoryBar/>
+    <TopCollection/>
+    <PromoBanner/>
+    <ServiceBar/>
+   <CombinedComponent/>
+  <Layout/>
+  <AdBanner/>
+  <Social/>
+  <Footer/>
+    </>
+  )
+}
+
+export default Home
